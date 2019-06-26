d2c3e7ff16ecabd739e7809f368d9863f608e663
Nancy Zitle <ancizj@hotmail.com>
2019-06-26 15:42:56 -0500

    Updates

---
+++ precache-manifest.cedc7d12487d85db912aedeba60cbb08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed7b1dc834e500d5893e23440bf6fe0",
+    "url": "/urbvan/index.html"
+  },
+  {
+    "revision": "753fdbd4b546823ea79d",
+    "url": "/urbvan/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "abdce2c0bdd422da7ad4",
+    "url": "/urbvan/static/js/2.770ee15e.chunk.js"
+  },
+  {
+    "revision": "753fdbd4b546823ea79d",
+    "url": "/urbvan/static/js/main.2c5d3af8.chunk.js"
+  },
+  {
+    "revision": "0425e3d58cd6b07e13d9",
+    "url": "/urbvan/static/js/runtime~main.4cf9dde7.js"
+  }
+]);
